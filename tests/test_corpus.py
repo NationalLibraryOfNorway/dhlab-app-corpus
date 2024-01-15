33b36aec6180f7351780f6e7e851d08cb00d6f8a
Lars Magne Tungland <larsmtungland@gmail.com>
2024-01-15 13:09:44 +0100

    test(corpus): add tests for corpus

---
+++ tests/test_corpus.py
@@ -0,0 +1,11 @@
+# Test that corpus works correctly
+import dhlab as dh
+
+class TestCorpus:
+    
+    def test_corpus_zero(self):
+        assert len(dh.Corpus()) == 0
+        
+    def test_corpus(self):
+        for doctype in dh.Corpus.doctypes:
+            assert len(dh.Corpus(doctype=doctype, limit=2)) == 2
